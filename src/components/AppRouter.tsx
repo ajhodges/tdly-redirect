8f75f72b29c8e5c7077dd438d14df5696f684c8f
Adam Hodges <adam.hodges@coinbase.com>
2025-04-22 18:41:42 -0400

    Updates

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -5,11 +5,11 @@ import Reset from './Reset';
 
 const AppRouter: React.FC = () => {
   return (
-    <BrowserRouter basename="/tdly-redirect">
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RedirectForm />} />
-        <Route path="/reset" element={<Reset />} />
-        <Route path="/simulator/new" element={<RedirectForm />} />
+        <Route path="/tdly-redirect" element={<RedirectForm />} />
+        <Route path="/tdly-redirect/reset" element={<Reset />} />
+        <Route path="/tdly-redirect/simulator/new" element={<RedirectForm />} />
       </Routes>
     </BrowserRouter>
   );
